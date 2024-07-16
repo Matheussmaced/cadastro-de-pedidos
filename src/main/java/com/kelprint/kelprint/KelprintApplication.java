729135da73b80de0d331ce325995bb459c2edd3d
Matheussmaced <mateusbyakuya3@gmail.com>
2024-07-16 11:29:49 -0300

    feat: first commit

---
+++ src/main/java/com/kelprint/kelprint/KelprintApplication.java
@@ -0,0 +1,13 @@
+package com.kelprint.kelprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelprint/kelprint/KelprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelprint.kelprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
