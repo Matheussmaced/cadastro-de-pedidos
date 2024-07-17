803855cf21679642729c515b5c5080b78fe3cde7
Matheussmaced <mateusbyakuya3@gmail.com>
2024-07-17 11:58:41 -0300

    feat: creating class client and order

---
+++ src/main/java/com/kelprint/kelprint/entity/Client.java
@@ -0,0 +1,85 @@
+package com.kelprint.kelprint.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import java.time.Instant;
+import java.util.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_clients")
+public class Client {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "client_Id")
+  private UUID clientId;
+
+  @Column(name = "name")
+  String name;
+
+  @CreationTimestamp
+  private Instant creationTimestamp;
+
+  @UpdateTimestamp
+  private Instant updateTimestamp;
+
+  @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonManagedReference
+  private List<Order> order;
+
+  public Client() {
+  }
+
+  public Client(UUID clientId, String name, Instant creationTimestamp, Instant updateTimestamp, List<Order> order) {
+    this.clientId = clientId;
+    this.name = name;
+    this.creationTimestamp = creationTimestamp;
+    this.updateTimestamp = updateTimestamp;
+    this.order = order;
+  }
+
+  public UUID getClientId() {
+    return clientId;
+  }
+
+  public void setClientId(UUID clientId) {
+    this.clientId = clientId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Instant getCreationTimestamp() {
+    return creationTimestamp;
+  }
+
+  public void setCreationTimestamp(Instant creationTimestamp) {
+    this.creationTimestamp = creationTimestamp;
+  }
+
+  public Instant getUpdateTimestamp() {
+    return updateTimestamp;
+  }
+
+  public void setUpdateTimestamp(Instant updateTimestamp) {
+    this.updateTimestamp = updateTimestamp;
+  }
+
+  public List<Order> getOrder() {
+    return order;
+  }
+
+  public void setOrder(List<Order> order) {
+    this.order = order;
+  }
+
+}
---
+++ src/main/java/com/kelprint/kelprint/entity/Order.java
@@ -0,0 +1,172 @@
+package com.kelprint.kelprint.entity;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "client_Id")
+  @JsonBackReference
+  private Client client;
+
+  @Column(name = "order_description")
+  private String orderDescription;
+
+  @Column(name = "amount")
+  private int amount;
+
+  @Column(name = "sizes")
+  private String sizes;
+
+  @Column(name = "kind_of_fabric")
+  private String kindOfFabric;
+
+  @Column(name = "type_of_collar")
+  private String typeOfCollar;
+
+  @Column(name = "comments")
+  private String comments;
+
+  @Column(name = "delivery_date")
+  private Date deliveryDate;
+
+  @Column(name = "finished")
+  private boolean finished;
+
+  @CreationTimestamp
+  private Instant creationTimestamp;
+
+  @UpdateTimestamp
+  private Instant updateTimestamp;
+
+  public Order() {
+  }
+
+  public Order(UUID id, Client client, String orderDescription, int amount, String sizes, String kindOfFabric,
+      String typeOfCollar, String comments, Date deliveryDate, boolean finished, Instant creationTimestamp,
+      Instant updateTimestamp) {
+    this.id = id;
+    this.client = client;
+    this.orderDescription = orderDescription;
+    this.amount = amount;
+    this.sizes = sizes;
+    this.kindOfFabric = kindOfFabric;
+    this.typeOfCollar = typeOfCollar;
+    this.comments = comments;
+    this.deliveryDate = deliveryDate;
+    this.finished = finished;
+    this.creationTimestamp = creationTimestamp;
+    this.updateTimestamp = updateTimestamp;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Client getClient() {
+    return client;
+  }
+
+  public void setClient(Client client) {
+    this.client = client;
+  }
+
+  public String getOrderDescription() {
+    return orderDescription;
+  }
+
+  public void setOrderDescription(String orderDescription) {
+    this.orderDescription = orderDescription;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public String getSizes() {
+    return sizes;
+  }
+
+  public void setSizes(String sizes) {
+    this.sizes = sizes;
+  }
+
+  public String getKindOfFabric() {
+    return kindOfFabric;
+  }
+
+  public void setKindOfFabric(String kindOfFabric) {
+    this.kindOfFabric = kindOfFabric;
+  }
+
+  public String getTypeOfCollar() {
+    return typeOfCollar;
+  }
+
+  public void setTypeOfCollar(String typeOfCollar) {
+    this.typeOfCollar = typeOfCollar;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  public Date getDeliveryDate() {
+    return deliveryDate;
+  }
+
+  public void setDeliveryDate(Date deliveryDate) {
+    this.deliveryDate = deliveryDate;
+  }
+
+  public boolean isFinished() {
+    return finished;
+  }
+
+  public void setFinished(boolean finished) {
+    this.finished = finished;
+  }
+
+  public Instant getCreationTimestamp() {
+    return creationTimestamp;
+  }
+
+  public void setCreationTimestamp(Instant creationTimestamp) {
+    this.creationTimestamp = creationTimestamp;
+  }
+
+  public Instant getUpdateTimestamp() {
+    return updateTimestamp;
+  }
+
+  public void setUpdateTimestamp(Instant updateTimestamp) {
+    this.updateTimestamp = updateTimestamp;
+  }
+
+}
